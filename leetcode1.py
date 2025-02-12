08c04ec9d6c51869970564d4020a5973a2fcdd5a
RAHUL <rahul507538@gmail.com>
2025-02-12 18:22:05 +0530

    Add files via upload

---
+++ leetcode1.py
@@ -0,0 +1,18 @@
+#Given a sorted array of distinct integers and a target value,
+#  return the index if the target is found.
+#  If not, return the index where it would be if it were inserted in order
+
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
