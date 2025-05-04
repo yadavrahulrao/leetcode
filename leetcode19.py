987469226620d237617a617a69c9b5f5542aa5b4
RAHUL <rahul507538@gmail.com>
2025-05-04 22:15:05 +0530

    Add files via upload

---
+++ leetcode19.py
@@ -0,0 +1,25 @@
+#Reverse Integer
+
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        result = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            digit = x % 10
+            x //= 10
+
+            # Check for overflow
+            if result > (INT_MAX - digit) // 10:
+                return 0
+
+            result = result * 10 + digit
+
+        return sign * result
