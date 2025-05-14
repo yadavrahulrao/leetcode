8b4ee4c57e70012b4fdb90c2a105e6d6ef9ab5b1
RAHUL <rahul507538@gmail.com>
2025-05-14 23:28:09 +0530

    Add files via upload

---
+++ leetcode22.py
@@ -0,0 +1,10 @@
+#Palindrome Number
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1]
