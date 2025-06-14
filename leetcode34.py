559d56850890ed893a6e19b3b8a23462adf8d806
RAHUL <rahul507538@gmail.com>
2025-06-14 21:02:12 +0530

    Add files via upload

---
+++ leetcode34.py
@@ -0,0 +1,25 @@
+# Longest Consecutive Sequence
+
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            # Only start sequence if num is the beginning of a sequence
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest = max(longest, current_streak)
+
+        return longest
