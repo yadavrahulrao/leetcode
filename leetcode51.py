effd5d33184e6e5bca40e5d5dcd087f7f92e1f72
RAHUL <rahul507538@gmail.com>
2025-07-14 21:55:38 +0530

    Add files via upload

---
+++ leetcode51.py
@@ -0,0 +1,23 @@
+#Valid Parentheses
+
+
+
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
