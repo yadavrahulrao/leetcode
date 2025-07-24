59144dee4a8042c27a70a6a3c39a9bc88d2e8f52
RAHUL <rahul507538@gmail.com>
2025-07-24 22:17:39 +0530

    Add files via upload

--- leetcode61.py
+++ leetcode61.py
@@ -5,7 +5,13 @@
 class Solution:
 
     def __init__(self, head: Optional[ListNode]):
+      self.arr = []
+      current = head
+      while current is not None:
+         self.arr.append(current.val)
+         current= current.next
         
 
     def getRandom(self) -> int:
+       return self.arr[random.randrange(len(self.arr))]
      
