184684bc0b34aa8ded8426554e67b6472c91276d
RAHUL <rahul507538@gmail.com>
2025-08-08 22:16:47 +0530

    Add files via upload

---
+++ leetcode73.py
@@ -0,0 +1,19 @@
+#739. Daily Temperatures
+
+
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []  # Stack stores indices of the temperatures
+
+        for i in range(n):
+            # While stack is not empty and current temp > temp at index on top of stack
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+            stack.append(i)
+
+        return answer
