bcd4d0716e68526c5f20c54198806342d7748b44
RAHUL <rahul507538@gmail.com>
2025-09-09 21:49:25 +0530

    Add files via upload

---
+++ leetcode104.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single_num = 0
+        
+      
+        for num in nums:
+           
+            single_num ^= num
+            
+     
+        return single_num
