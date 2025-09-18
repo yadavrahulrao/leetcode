87bbeedca5f31aa58415c03c3ff07dd09914bd94
RAHUL <rahul507538@gmail.com>
2025-09-18 21:26:41 +0530

    Add files via upload

---
+++ leetcode108.py
@@ -0,0 +1,42 @@
+#18. 4Sum
+from typing import List
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        res = []
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # Skip duplicates for the first number
+
+            for j in range(i + 1, n):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue  # Skip duplicates for the second number
+
+                left = j + 1
+                right = n - 1
+
+                while left < right:
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+
+                        # Skip duplicates for the third number
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        # Skip duplicates for the fourth number
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+
+                        left += 1
+                        right -= 1
+
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
+
+        return res
