715293f1300ff1e346ae553f65370e3ddb1e684d
yadavrahulrao <rahul507538@gmail.com>
2026-01-20 21:40:00 +0530

    159

---
+++ leetcode159.py
@@ -0,0 +1,31 @@
+#63. Unique Paths II
+from typing import List
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        
+        # If start or end is blocked
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1] == 1:
+            return 0
+        
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+        
+        # First column
+        for i in range(1, m):
+            if obstacleGrid[i][0] == 0:
+                dp[i][0] = dp[i-1][0]
+        
+        # First row
+        for j in range(1, n):
+            if obstacleGrid[0][j] == 0:
+                dp[0][j] = dp[0][j-1]
+        
+        # Fill the rest
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m-1][n-1]
