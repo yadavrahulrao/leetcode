b150c3f2ee873d54664568a333c3350721174fbd
yadavrahulrao <rahul507538@gmail.com>
2026-04-14 22:54:43 +0530

    203

---
+++ leetcode203.py
@@ -0,0 +1,14 @@
+#997. Find the Town Judge
+class Solution:
+    def findJudge(self, n: int, trust: list[list[int]]) -> int:
+        score = [0] * ( n + 1)
+
+        for a , b in trust:
+          score[a] -= 1
+          score[b] += 1
+        
+        for i in range (1,n+1):
+          if score[i] == n-1 :
+            return i 
+        
+        return -1
