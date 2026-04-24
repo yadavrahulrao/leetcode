7d682f91f0d9effc6b9db7ccdb30db6793566e09
yadavrahulrao <rahul507538@gmail.com>
2026-04-24 22:08:50 +0530

    1

---
+++ codewar1.py
@@ -0,0 +1,11 @@
+# Find the odd int
+
+
+# we have to find  that which num. in the given list is coming odd no of times . duplicates 
+# also aloowd 
+
+def find_it(seq):
+    result = 0
+    for num in seq :
+      result ^= i
+    return result
