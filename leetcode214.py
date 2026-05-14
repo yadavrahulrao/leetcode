d25045643d1cb0aaf397fe00441d525771a526a9
yadavrahulrao <rahul507538@gmail.com>
2026-05-14 23:45:07 +0530

    214

---
+++ leetcode214.py
@@ -0,0 +1,21 @@
+#424. Longest Repeating Character Replacement
+from collections import Counter
+class Solution(object):
+    def characterReplacement(self, s, k):
+      count = Counter()
+      left = 0
+      j = 0
+      for right , i in enumerate(s):
+        count[i] += 1
+        # j = max(j,count[i])
+        # if right - left + 1 - j > k :
+        #   count[s[left]] -= 1
+        #   left += 1
+      return count
+
+
+s = Solution()
+print(s.characterReplacement("ABBBBABBBAAAAACCCCC",2))
+            
+
+        
