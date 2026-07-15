167fb52dcf64086706ddc64789c9b040d48e70b9
Rahul <rahul507538@gmail.com>
2026-07-15 22:59:18 +0530

    246

---
+++ leetcode246.py
@@ -0,0 +1,13 @@
+#190. Reverse Bits
+
+
+
+class Solution:
+    def reverseBits(self, n):
+        ans = 0
+
+        for _ in range(32):
+            ans = (ans << 1) | (n & 1)
+            n >>= 1
+
+        return ans
