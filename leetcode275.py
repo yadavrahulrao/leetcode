8de320a50577dd5eabfb7913f3194470cb5976f0
yadavrahulrao <rahul507538@gmail.com>
2026-08-13 22:14:40 +0530

    275

---
+++ leetcode275.py
@@ -0,0 +1,9 @@
+#153. Find Minimum in Rotated Sorted Array
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return min(nums)
