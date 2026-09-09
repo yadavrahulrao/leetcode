2b373d470173a19768b501b6585174738dc420ee
yadavrahulrao <rahul507538@gmail.com>
2026-09-09 21:41:16 +0530

    299

---
+++ leetcode299.py
@@ -0,0 +1,14 @@
+#236. Lowest Common Ancestor of a Binary Tree
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if root is None or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
