7ebd50962b9e5f40507b9fbc6de3c9ace2bbb9d8
yadavrahulrao <rahul507538@gmail.com>
2026-09-21 23:51:01 +0530

    310

---
+++ leetcode310.py
@@ -0,0 +1,12 @@
+#1876. Substrings of Size Three with Distinct Characters
+
+
+class Solution:
+    def countGoodSubstrings(self, s):
+        count = 0
+
+        for i in range(len(s) - 2):
+            if s[i] != s[i + 1] and s[i] != s[i + 2] and s[i + 1] != s[i + 2]:
+                count += 1
+
+        return count
