21f7445d98e015c64ebe8276d3ac1fac7aca5301
yadavrahulrao <rahul507538@gmail.com>
2026-09-22 22:54:48 +0530

    4

---
+++ geek4.py
@@ -0,0 +1,15 @@
+#Prime Number
+class Solution:
+    def isPrime(self, n):
+        # code here
+        if n <= 1 :
+            return False
+        for i in range(2,int(n**0.5)+1):
+            if n % i == 0:
+                return False
+        return True
+
+obj = Solution()
+print(obj.isPrime(4))
+            
+        
